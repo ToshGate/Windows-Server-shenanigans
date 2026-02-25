583c66e63afc5934551eab1071b9522dd258570a
ToshGate <35961002+ToshGate@users.noreply.github.com>
2026-02-25 15:20:15 +0000

    Add files via upload

---
+++ Ativar templates.ps1
@@ -0,0 +1,18 @@
+﻿# Check if running as Administrator
+If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    # Relaunch script as Administrator
+    $arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+    Start-Process powershell -Verb RunAs -ArgumentList $arguments
+    exit
+}
+
+# Script continues here with Admin privileges
+
+DISM /online /Get-CurrentEdition
+
+DISM /online /Get-TargetEditions
+
+DISM /online /Set-Edition:ServerDatacenter /ProductKey:XXXXX-XXXXX-XXXXX-XXXXX-XXXXX /AcceptEula
+
+pause
